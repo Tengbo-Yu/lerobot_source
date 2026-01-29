825775a6e9fcde33945313656007db08c5aabf7b
Tengbo-Yu <ytb23@mails.tsinghua.edu.cn>
2026-01-29 04:11:08 +0000

    update

--- scripts/train_2.sh
+++ scripts/train_2.sh
@@ -44,9 +44,9 @@ CUDA_VISIBLE_DEVICES=${GPU_ID} python src/lerobot/scripts/lerobot_train.py \
   --dataset.root=/media/raid/workspace/tengbo/any4lerobot/data/lerobot_data/InterceptMedium-v0/mikasa_dataset_1.0.0_lerobot \
   --dataset.repo_id=JackYuuuu/InterceptMedium-v0 \
   --policy.repo_id=JackYuuuu/SmolVLA_InterceptMedium_100 \
-  --policy.type=groot \
+  --policy.type=smolvla \
   --output_dir=outputs/train/SmolVLA_InterceptMedium-v0\
-  --job_name=Groot_InterceptMedium-v0 \
+  --job_name=SmolVLA_InterceptMedium-v0 \
   --policy.device=cuda \
   --wandb.enable=true
 
